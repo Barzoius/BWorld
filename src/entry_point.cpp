3a48e9c7063a6b2e6810df1c57fb5fdc5da622bd
Barzoius <mecca.rares@gmail.com>
2026-02-15 23:51:27 +0200

    fixed submodule

--- src/entry_point.cpp
+++ src/entry_point.cpp
@@ -1,8 +1,31 @@
 #include "Renderer.hpp"
+#include "GLFW/glfw3.h"
+
 
 int main()
 {
     f();
+     if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Minimal GLFW Window", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
